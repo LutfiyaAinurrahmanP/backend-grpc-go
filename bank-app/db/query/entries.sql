f745a3988614b96d06cd672c2df8e7a8df7d876d
Lutfiya Ainurrahman Prasetyo <lutfiyapr.creative@gmail.com>
2025-12-06 23:56:49 +0700

    feat: add entries and transfer sql for sqlc generate

---
+++ bank-app/db/query/entries.sql
@@ -0,0 +1,27 @@
+-- name: GetEntries :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+WHERE account_id $1
+ORDER BY id
+LIMIT $2 OFFSET $3;
+
+-- name: CreateEntries :one
+INSERT INTO entries (
+  account_id,
+  amount
+) VALUES (
+  $1, $2
+) RETURNING *;
+
+-- name: UpdateEntries :exec
+UPDATE entries
+  set account_id = $2,
+  amount = $3
+WHERE id = $1;
+
+-- name: DeleteEntries :exec
+DELETE FROM entries
+WHERE id = $1;
