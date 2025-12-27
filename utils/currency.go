9ec01e6acd7796b11540054c24bfd7ada0f46293
Lutfiya Ainurrahman Prasetyo <lutfiyapr.creative@gmail.com>
2025-12-27 12:09:56 +0700

    feat: add custom validator

---
+++ utils/currency.go
@@ -0,0 +1,17 @@
+package utils
+
+// Constant for all supported currency
+const (
+	USD = "USD"
+	EUR = "EUR"
+	IDR = "IDR"
+)
+
+// IsSupportedCurrency returns true if the currency is supported
+func IsSupportedCurrency(currency string) bool {
+	switch currency {
+	case USD, EUR, IDR:
+		return true
+	}
+	return false	
+}
